bf594d68ae4b0667b7bde7d2e39c0468370d7e31
joaogt01 <joao.dev06@gmail.com>
2026-09-08 22:24:30 -0300

    feat: cria CategoriaException para substituir RuntimeException genérica

---
+++ src/main/java/com/loja/exceptions/CategoriaException.java
@@ -0,0 +1,12 @@
+package com.loja.exceptions;
+
+public class CategoriaException extends RuntimeException {
+
+    public CategoriaException(String mensagem) {
+        super(mensagem);
+    }
+
+    public CategoriaException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
+    }
+}
