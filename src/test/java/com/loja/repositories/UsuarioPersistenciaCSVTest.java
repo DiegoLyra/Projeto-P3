b757571533f8402270e2f116f2a1121cf8729354
joaogt01 <joao.dev06@gmail.com>
2026-09-19 16:18:58 -0300

    chore: renomeia classes Teste para Test

--- src/test/java/com/loja/repositories/UsuarioPersistenciaCSVTest.java
+++ src/test/java/com/loja/repositories/UsuarioPersistenciaCSVTest.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UsuarioPersistenciaCSVTeste {
+class UsuarioPersistenciaCSVTest {
 
     @TempDir
     Path tempDir;
